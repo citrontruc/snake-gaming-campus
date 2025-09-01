e6c89954821119ad087b65c7884513277a77298c
clement lion <clement.lionceau@gmail.com>
2025-09-01 20:13:26 +0200

    added template for levels

--- src/Scenes/levels/Level.cs
+++ src/Scenes/levels/Level.cs
@@ -1,1 +1,48 @@
 /* An object to handle a level scene in which the player can evolve. */
+
+using System.Numerics;
+using Raylib_cs;
+
+public abstract class Level : Scene
+{
+    private Color _backGroundColor = Color.Black;
+
+    public Level()
+    {
+        
+    }
+
+    #region Get Important information
+    
+    #endregion
+
+    #region Set important Characteristics
+    public void SetBackgroundcharacteristics(Color backGroundColor)
+    {
+        _backGroundColor = backGroundColor;
+    }
+    #endregion
+
+    #region Update
+    public override void Update(float deltaTime)
+    {
+
+    }
+    #endregion
+
+    #region Draw functions
+    /// <summary>
+    /// We let the entity handler handle drawing.
+    /// </summary>
+    override public void Draw()
+    {
+        DrawBackground();
+        //DrawEntity();
+    }
+
+    public void DrawBackground()
+    {
+        Raylib.ClearBackground(_backGroundColor);
+    }
+    #endregion
+}
---
+++ src/Scenes/levels/Level1.cs
@@ -0,0 +1,14 @@
+/* First level of the game with two snakes and two fruits to grab.*/
+
+public class Level1 : Level
+{
+    public override void Load()
+    {
+
+    }
+
+    public override void Unload()
+    {
+
+    }
+}
