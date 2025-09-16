742118da35b00918ea3374ce21457b0617e01ad8
clement lion <clement.lionceau@gmail.com>
2025-09-16 17:18:49 +0200

    added failed test and made tests mandatory for main

--- Game.Tests/UnitTest1.cs
+++ Game.Tests/UnitTest1.cs
@@ -9,4 +9,11 @@ public class UnitTest1
         int test = 6;
         Assert.IsTrue(test == 6);
     }
+
+    [TestMethod]
+    public void TestMethod2()
+    {
+        int test = 5;
+        Assert.IsTrue(test == 6);
+    }
 }
