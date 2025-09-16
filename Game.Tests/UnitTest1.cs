862eb7ef62777d3f94c49a9242e405efbdf8f31b
citrontruc <122884693+citrontruc@users.noreply.github.com>
2025-09-16 16:45:33 +0200

    20250916 tests (#3)
    Added test folder and corrected workflow in order to avoid disruption due to the change of file directories.

---
+++ Game.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace Game.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        int test = 6;
+        Assert.IsTrue(test == 6);
+    }
+}
